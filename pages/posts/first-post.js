8588370a41b66ba13187e453d5615298365fdf66
shunak <shuna966@gmail.com>
2021-08-27 18:49:03 +0900

    wip

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,18 +1,23 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
 	return (
-		<>
+		<Layout>
 			<Head>
 				<title>First Post</title>
 			</Head>
 			<h1>First Post</h1>
 			<h2>
-				<Link href="/">
+				My precious days are here.
+				{/* <Link href="/">
 					<a>Back to home</a>
-				</Link>
+				</Link> */}
 			</h2>
-		</>
+			<p>
+				Life is a journey, and I'm a part of it.
+			</p>
+		</Layout>
 	)
 }
