8fa21a7caf87d368d3d3e182e5c1614f4c286282
shunak <shuna966@gmail.com>
2021-09-08 16:40:16 +0900

    Introduced TypeScript

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import '../styles/global.css'
+import { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+// import '../styles/global.css'
+
+// export default function App({Component, pageProps}) {
+//   return <Component {...pageProps} />
+// }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,9 @@
-export default function (req, res) {
-	res.status(200).json({text: 'Hello World!'});
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default (_: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ text: 'Hello' })
 }
+
+// export default function (req, res) {
+// 	res.status(200).json({text: 'Hello World!'});
+// }
